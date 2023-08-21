f6d0115f8e5a1f20f7adc8f92695e72255424f5f
Eran Keydar <eran@swimm.io>
2023-08-21 17:04:12 +0300

    break

--- ex1.cpp
+++ ex1.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 // One function works for all data types. This would work
 // even for user defined types if operator '>' is overloaded
-template <typename T> T myaMax(T x, T y)
 {
 	return (x > y) ? x : y;
 }
